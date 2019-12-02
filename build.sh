ef4fc7e6a63a0e94f9296614d43297e3e7069ba2
prashanth <prashanthhn2509@gmail.com>
2019-12-02 19:37:45 +0530

    Added server, client and build.sh. Server and client are communication with each other. Need to understand how it works.

---
+++ build.sh
@@ -0,0 +1,28 @@
+log(){
+    echo "`date ` ${1}"
+}
+
+log "Cleaning bin ..."
+cd ./bin
+rm -rf ./*
+cd ../
+log "Done cleaning bin."
+
+log "Building protocol ..."
+protoc -I api/ \
+    -I${GOPATH}/src \
+    --go_out=plugins=grpc:api \
+    api/api.proto
+log "Done building protocol."
+
+log "Formatting code ..."
+gofmt -s -w -l .
+log "Done formatting code."
+
+log "Building server ..."
+go build -i -v -o ./bin/server ./server/
+log "Done building server."
+
+log "Building client ..."
+go build -i -v -o ./bin/client ./client/
+log "Done building client."
